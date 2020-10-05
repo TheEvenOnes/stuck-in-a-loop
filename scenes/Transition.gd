97018914d8c0c170e82046dec01a45e19f6c6b43
Petar Šlat <petar.slat@gmail.com>
2020-10-06 01:18:46 +0200

    Change default scene

--- scenes/Transition.gd
+++ scenes/Transition.gd
@@ -10,7 +10,8 @@ onready var _anim_player := $AnimationPlayer
 func transition_to(_next_scene := next_scene_path) -> void:
 	_anim_player.play("Fade")
 	yield(_anim_player, "animation_finished")
+	get_tree().change_scene(_next_scene)
+	
 	yield(get_tree().create_timer(0.5), "timeout")
 	_anim_player.play_backwards("Fade")
 	yield(_anim_player, "animation_finished")
-	get_tree().change_scene(_next_scene)
